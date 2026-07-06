7df307c791bff61acb45ebfbc4ba419efc096795
Lukas Short <stockente.mail@gmail.com>
2026-07-06 14:20:39 -0700

    isometric view

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { genTerrain , genHeights } from "./terrain/utils.js";
+import { genTerrain , genHeights , getNeighbor } from "./terrain/utils.js";
 import { Tile } from "./Tile.js";
 
 const canvas = document.getElementById("canvas");
@@ -8,7 +8,17 @@ let mousex = 0;
 let mousey = 0;
 let hover = 0;
 
-const tile_width = 50;
+let panx = 0;
+let pany = 0;
+let zoom = 1;
+
+let panleft = false;
+let panright = false;
+let panup = false;
+let pandown = false;
+
+const tile_width = 30;
+const tile_height = 15;
 const tile_gap = 0;
 const full_tile = tile_width + tile_gap;
 
@@ -22,24 +32,37 @@ document.addEventListener("mousemove", (e) => {
     let hx = (mousex - mousex % full_tile)
     let hy = (mousey - mousey % full_tile);
     if(mousex >= 0 && mousey >= 0                                                   //Not too far up and left
-    && mousex < world_width*tile_width && mousey < world_height*(tile_width+tile_gap)          //Not too far down and right
+    && mousex < chunk_width*tile_width && mousey < chunk_height*(tile_width+tile_gap)          //Not too far down and right
     && mousex < hx + tile_width && mousey < hy + tile_width)                        //Not in the gaps
-        {hover = getIndex([hx/full_tile, hy/full_tile])}
+        {hover = getIndex({x:hx/full_tile, y:hy/full_tile}, chunk_width)}
     else {hover = -1}
 });
 document.addEventListener("mousedown", () => {
-    if(hover >= 0) {genTerrain(hover, world, world_width, world_height);}
+    if(hover >= 0) {genTerrain(hover, chunk, chunk_width, chunk_height);}
 });
 
-const world_width = 8;
-const world_height = 8;
+const chunk_width = 8;
+const chunk_height = 8;
+
+const world_width = 4;
+const world_height = 4;
 
 //Initialize chunk
-let world = {
-    h1: new Uint8Array(world_width * world_height)
+let world = [];
+for(let i = 0; i < world_width*world_height; i++) {
+    let chunk = {
+        x: i % world_width,
+        y: (i - i % world_width) /world_height,
+        h1: new Uint8Array(chunk_width * chunk_height)
+    }
+    const p = getPos(i, world_width, world_height);  
+    const pn1 = {x: p.x-1, y: p.y};
+    const pn2 = {x: p.x, y: p.y-1};
+    let chunkn1 = world[getIndex(pn1, world_width)];
+    let chunkn2 = world[getIndex(pn2, world_width)];
+    genHeights(chunk, chunkn1, chunkn2, chunk_width, chunk_height);
+    world.push(chunk);
 }
-genHeights(world, world_width, world_height);
-console.log(world);
 
 //last time
 let lt = window.performance.now();
@@ -55,36 +78,83 @@ function step(timestamp) {
 
 function draw(dt) {
     renderTerrain();
+    update();
 }
 
-function getPos(i) {
-    return [i % world_width,(i - i % world_width)/world_height];
+function update() {
+    if(panleft) {panx -= 2}
+    if(panright) {panx += 2}
+    if(panup) {pany -= 1}
+    if(pandown) {pany += 1}
 }
 
-function getIndex([x, y]) {
-    return x + y * world_width;
+function getPos(i, chunk_width, chunk_height) {
+    return {x: i % chunk_width, y: (i - i % chunk_width)/chunk_height};
+}
+
+function getIndex(p, chunk_width) {
+    return p.x + p.y * chunk_width;
 }
 function renderTerrain() {
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-    for(let i = 0; i < world_width*world_height; i++) {
-        
-        let v = world.h1[i] *1.5;
-        let p = getPos(i);
-        let x = p[0]*full_tile;
-        let y = p[1]*full_tile;
-        ctx.fillStyle = `rgb(${v},${v},${v})`;
-        if(hover == i) {ctx.fillStyle = "rgba(146, 36, 36, 0.5)"}
-        ctx.fillRect(x, y, tile_width, tile_width);
+    for(let chunk of world) {
+        let o = convertTilePos({x: chunk.x, y: chunk.y}, chunk_width*tile_width, chunk_width*tile_height, {x: 0, y: 0});
+        for(let i = 0; i < chunk_width*chunk_height; i++) {
+            let v = chunk.h1[i] *1;
+            let p = getPos(i, chunk_width, chunk_height);
+            let t = convertTilePos(p, tile_width, tile_height, o, v);
+            // let x = panx + canvas.clientWidth/2 - tile_width/2 +(p.x*(full_tile/2))+(-p.y*(full_tile/2));
+            // let y = pany + canvas.clientHeight/2 - (tile_width/2)*chunk_width + (p.x*(h/2))+(p.y*(h/2)) - v/5 + 100;
+            ctx.beginPath();
+            ctx.moveTo(t.x, t.y);                          // top point
+            ctx.lineTo(t.x + tile_width/2, t.y + tile_height/2);    // right point
+            ctx.lineTo(t.x + tile_width/2, t.y + tile_height);
+            ctx.lineTo(t.x, t.y + tile_height*1.5);                     // bottom point
+            ctx.lineTo(t.x - tile_width/2, t.y + tile_height);
+            ctx.lineTo(t.x - tile_width/2, t.y + tile_height/2);    // left point
+            ctx.closePath();
+            ctx.fillStyle = `rgb(${v},${v},${v})`;
+            ctx.fill();
+            if(hover == i) {ctx.fillStyle = "rgba(146, 36, 36, 0.5)"}
+            //ctx.fillRect(x, y, tile_width, tile_width);
+        }
     }
+}
+function convertTilePos(p, w, h, o, v = 0) {
+    let x = (o.x) + (panx) + (canvas.clientWidth/4) + (p.x*(w/2))+(-p.y*(w/2));
+    let y = (o.y) + (pany) + (canvas.clientHeight/2 + (-250)) + (p.x*(h/2))+(p.y*(h/2)) - (v/5);
+    return {x: x, y: y}
 }
 function keyDownHandler(e) {
     switch(e.key.toLowerCase()) {
-        
+        case "a":
+            panleft = true;
+            break;
+        case "d":
+            panright = true;
+            break;
+        case "w":
+            panup = true;
+            break;
+        case "s":
+            pandown = true;
+            break;
     }
 }
 function keyUpHandler(e) {
     switch(e.key.toLowerCase()) {
-        
+        case "a":
+            panleft = false;
+            break;
+        case "d":
+            panright = false;
+            break;
+        case "w":
+            panup = false;
+            break;
+        case "s":
+            pandown = false;
+            break;
     }
 }
 function keyPressHandler(e) {
--- terrain/utils.js
+++ terrain/utils.js
@@ -1,41 +1,50 @@
 function genTerrain(start, chunk, chunk_width, chunk_height) {
-    chunk[start] = 1;
-    let open = [];
-    let closed = new Set();
-    open.push(start);
+    // chunk[start] = 1;
+    // let open = [];
+    // let closed = new Set();
+    // open.push(start);
 
-    const dirs = [
-        {x: -1, y: 0},
-        {x: 1, y: 0},
-        {x: 0, y: 1},
-        {x: 0, y: -1},
-    ];
-    let steps = 0;
-    while(open.length > 0) {
-        steps ++;
-        open.sort((a, b) => a - b);
-        let current = open.shift();
-        for(let d of dirs) {
-            const r = Math.random().toFixed(3);
-            const g = getDist(start, current, chunk_width);
-            if(r < 0 + g/3) {continue}
-            let n = getNeighbor(current, d.x, d.y, chunk_width, chunk_height);
-            if(n!==false && !closed.has(n) && !open.includes(n)) {
-                open.push(n);
-                chunk[n] = 1;
-            }
-        }
-        closed.add(current);
-    }
+    // const dirs = [
+    //     {x: -1, y: 0},
+    //     {x: 1, y: 0},
+    //     {x: 0, y: 1},
+    //     {x: 0, y: -1},
+    // ];
+    // let steps = 0;
+    // while(open.length > 0) {
+    //     steps ++;
+    //     open.sort((a, b) => a - b);
+    //     let current = open.shift();
+    //     for(let d of dirs) {
+    //         const r = Math.random().toFixed(3);
+    //         const g = getDist(start, current, chunk_width);
+    //         if(r < 0 + g/3) {continue}
+    //         let n = getNeighbor(current, d.x, d.y, chunk_width, chunk_height);
+    //         if(n!==false && !closed.has(n) && !open.includes(n)) {
+    //             open.push(n);
+    //             chunk[n] = 1;
+    //         }
+    //     }
+    //     closed.add(current);
+    // }
 }
 
-// For now, single chunk, no crossing into other chunks
-function getNeighbor(start, dx, dy, chunk_width, chunk_height) {
-    const p = getPos(start, chunk_width);       //Position of the origin
+//Returns the value at the tile, not the index in order for cross chunk neighbors
+function getNeighbor(start, dx, dy, chunk, chunkn1, chunkn2, chunk_width, chunk_height) {
+    let ochunk = 0;     // Origin Chunk, 0 meaning the regular chunk, 1 is chunkn1, 2 is chunkn2
+    const p = getPos(start, chunk_width);  
     const pn = {x: p.x+dx, y: p.y+dy};          //Position of the neighbor
-    if(pn.x < 0 || pn.y < 0 ||pn.x >= chunk_width || pn.y >= chunk_height) {return false}  //If on a border, return false
+    if(pn.x < 0 && chunkn1) {
+        pn.x = chunk_width-1;
+        ochunk = 1;
+    } else if (pn.x < 0) {return false}
+    if(pn.y < 0 && chunkn2) {
+        pn.y = chunk_height-1;
+        ochunk = 2;
+    } else if (pn.y < 0) {return false}  //If on a border, return false
+    let index = getIndex(pn, chunk_width, chunk_height);
     
-    return getIndex(pn, chunk_width, chunk_height);
+    return {i: index, o: ochunk};
 }
 
 function getPos(index, chunk_width) {
@@ -56,18 +65,31 @@ function getDist(a, b, chunk_width) {
     return Math.hypot(dx, dy);
 }
 
-function genHeights(chunk, chunk_width, chunk_height) {
+function genHeights(chunk, chunkn1, chunkn2, chunk_width, chunk_height) {
     let start = 100 + Math.floor(Math.random() * 10);
     for(let i = 0; i < chunk_width * chunk_height; i++) {
-        const p1 = getNeighbor(i, -1, 0, chunk_width, chunk_height); //Neighbor to the left
-        const p2 = getNeighbor(i, 0, -1, chunk_width, chunk_height);  //Neighbor to the top
-        
-        let parent = (p1!== false && p2!== false ? (chunk.h1[p1]+chunk.h1[p2])/2 : (p1==false&&p2!==false? chunk.h1[p2] : (p2==false&&p1!==false ? chunk.h1[p1] : start)));
-        //console.log(p1, p2, parent, i);
-        
-        chunk.h1[i] = parent - (Math.random() * 30) + 15;
-        console.log(i, p1, p2, chunk.h1[i], parent)
+        const p1 = getNeighbor(i, -1, 0, chunk, chunkn1, chunkn2, chunk_width, chunk_height); //Neighbor to the left
+        const p2 = getNeighbor(i, 0, -1, chunk, chunkn1, chunkn2, chunk_width, chunk_height);  //Neighbor to the top
+        let v1 = 0;
+        let v2 = 0;
+        switch(p1.o) {
+            case 0: v1 = chunk.h1[p1.i]; break;
+            case 1: v1 = chunkn1.h1[p1.i]; break;
+            case 2: v1 = chunkn2.h1[p1.i]; break;
+        }
+        switch(p2.o) {
+            case 0: v2 = chunk.h1[p2.i]; break;
+            case 1: v2 = chunkn1.h1[p2.i]; break;
+            case 2: v2 = chunkn2.h1[p2.i]; break;
+        }
+        // if(p1== false || p2 == false) {console.log(p1, p2)}
+        let parent = (p1!== false && p2!== false ? (v1+v2)/2 : (p1==false&&p2!==false? v2 : (p2==false&&p1!==false ? v1 : start)));
+        let dif = 40;
+        let r = parent - (Math.random() * dif) + (dif/2);
+        if(r <= 0) {r = 1}
+        if(r >= 200) {r = 200;}
+        chunk.h1[i] = r;
     }
 }
 
-export { genTerrain , genHeights };
+export { genTerrain , genHeights , getNeighbor };
